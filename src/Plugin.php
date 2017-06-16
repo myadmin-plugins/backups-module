b4bc8edf9be706decf5606542ee201675925d0c2
Joe Huss <detain@interserver.net>
2017-06-16 08:13:54 -0400

    updates to the plugin migrating the varios data in the myadmin.php file into the Plugin class itself so i can then phase out the myadmin.php file and then a developer only needs to setup this one simple file to setup a new plugin.

--- src/Plugin.php
+++ src/Plugin.php
@@ -6,7 +6,21 @@ use Symfony\Component\EventDispatcher\GenericEvent;
 
 class Plugin {
 
+	public static $name = 'Backup Services Module';
+	public static $description = 'Allows selling of Backup Services Module';
+	public static $help = '';
+	public static $module = 'backups';
+	public static $type = 'module';
+
+
 	public function __construct() {
+	}
+
+	public static function Hooks() {
+		return [
+			'backups.load_processing' => ['Detain\MyAdminBackups\Plugin', 'Load'],
+			'backups.settings' => ['Detain\MyAdminBackups\Plugin', 'Settings'],
+		];
 	}
 
 	public static function Load(GenericEvent $event) {
