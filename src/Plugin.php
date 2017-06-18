34b4ff71b4e5a450b6112e07474fd8f6b0acc025
Joe Huss <detain@interserver.net>
2017-06-18 01:47:08 -0400

    fixs to camelCase for psr

--- src/Plugin.php
+++ src/Plugin.php
@@ -19,7 +19,7 @@ class Plugin {
 	public static function getHooks() {
 		return [
 			'backups.load_processing' => [__CLASS__, 'Load'],
-			'backups.settings' => [__CLASS__, 'Settings'],
+			'backups.settings' => [__CLASS__, 'getSettings'],
 		];
 	}
 
@@ -27,7 +27,7 @@ class Plugin {
 
 	}
 
-	public static function Settings(GenericEvent $event) {
+	public static function getSettings(GenericEvent $event) {
 		$settings = $event->getSubject();
 		$settings->add_dropdown_setting('backups', 'General', 'outofstock_backups', 'Out Of Stock Backups', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_BACKUPS'), array('0', '1'), array('No', 'Yes', ));
 	}
