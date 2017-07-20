d16f31041765f598d7161b7aef77ca5a1dc9ff3d
Joe Huss <detain@interserver.net>
2017-07-20 01:29:52 -0400

    changing to single quotes

--- src/Plugin.php
+++ src/Plugin.php
@@ -72,7 +72,7 @@ class Plugin {
 				$settings = get_module_settings(self::$module);
 				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
 				$db = get_module_db(self::$module);
-				$db->query("update ".$settings['TABLE']." set ".$settings['PREFIX']."_status='pending-setup' where ".$settings['PREFIX']."_id='{$serviceInfo[$settings['PREFIX'].'_id']}'", __LINE__, __FILE__);
+				$db->query('update ' . $settings['TABLE']. ' set ' . $settings['PREFIX']."_status='pending-setup' where ". $settings['PREFIX']."_id='{$serviceInfo[$settings['PREFIX'].'_id']}'", __LINE__, __FILE__);
 				$GLOBALS['tf']->history->add($settings['PREFIX'], 'change_status', 'pending-setup', $serviceInfo[$settings['PREFIX'].'_id'], $serviceInfo[$settings['PREFIX'].'_custid']);
 				$GLOBALS['tf']->history->add(self::$module.'queue', $serviceInfo[$settings['PREFIX'].'_id'], 'initial_install', '', $serviceInfo[$settings['PREFIX'].'_custid']);
 				$smarty = new \TFSmarty;
