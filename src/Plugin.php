07fb76559a96d59f9761a53222e3000030003ff4
Joe Huss <detain@interserver.net>
2018-01-17 11:26:07 -0500

    image updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -21,7 +21,7 @@ class Plugin {
 		'USE_REPEAT_INVOICE' => TRUE,
 		'USE_PACKAGES' => TRUE,
 		'BILLING_DAYS_OFFSET' => 0,
-		'IMGNAME' => 'servers_48.png',
+		'IMGNAME' => 'network-drive.png',
 		'REPEAT_BILLING_METHOD' => PRORATE_BILLING,
 		'DELETE_PENDING_DAYS' => 45,
 		'SUSPEND_DAYS' => 14,
